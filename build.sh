edc1d7a37639b388944ee9f342c3a79b596be49e
mochaaP <git@mochaa.ws>
2021-12-12 09:25:50 +0800

    fix(build): config variants build only once

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 if [[ -e $1/$2.cfg ]]; then
   docker buildx build $1 -t frogging-build/$1:latest --build-arg BUILD_CONFIG=$2.cfg
+else
+  docker buildx build $1 -t frogging-build/$1:latest
 fi
-
-docker buildx build $1 -t frogging-build/$1:latest
 
 docker create --name frogging-$1 frogging-build/$1:latest
 
