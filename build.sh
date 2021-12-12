671cfc01b38f44d5e2e9f5a91e6423c180865a28
mochaaP <git@mochaa.ws>
2021-12-12 09:14:59 +0800

    feat(build): config variants

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [[ -e $1/$2.cfg ]]; then
+  docker buildx build $1 -t frogging-build/$1:latest --build-arg BUILD_CONFIG=$2.cfg
+fi
+
 docker buildx build $1 -t frogging-build/$1:latest
 
 docker create --name frogging-$1 frogging-build/$1:latest
